a2f3e599537c8c5564d2b41d93cf20afa1a231f3
smguggen <sguggenh@amazon.com>
2024-11-10 11:30:43 -0600

    Add start file

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+STACK_OPERATION=$1
+npm install;
+if [[ "$STACK_OPERATION" == "create" || "$STACK_OPERATION" == "update" ]]; then
+    cdk bootstrap
+    cdk deploy --require-approval never --all
+elif [ "$STACK_OPERATION" == "delete" ]; then
+    cdk destroy --force --all
+else
+    echo "Invalid stack operation!"
+    exit 1
+fi
