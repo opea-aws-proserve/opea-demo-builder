c9e701cf847d284e08bcdbeffa1d69b856cdc93b
Steven Guggenheimer <sguggenh@amazon.com>
2024-11-16 21:15:01 +0000

    Remove nested stacks from start script

--- lib/app/start.sh
+++ lib/app/start.sh
@@ -9,8 +9,8 @@ else
     export OPEA_DEMO_BUILDER="https://github.com/opea-aws-proserve/opea-demo-builder.git"
     cdk synth --quiet
     cdk bootstrap
-    cdk deploy --require-approval never OpeaEksStack
-   # cdk deploy --require-approval never --all
+   # cdk deploy --require-approval never OpeaEksStack
+    cdk deploy --require-approval never --all
     export WORKSHOP_BUCKET=$(aws ssm get-parameter --name workshop-bucket --query Parameter.Value --output text)
     aws s3 sync "." "s3://$WORKSHOP_BUCKET/opea-demo-builder"
     aws s3 sync "./cdk.out/OpeaChatQnAStack.template.json" "s3://$WORKSHOP_BUCKET/cloudformation/OpeaChatQnAStack.json"
