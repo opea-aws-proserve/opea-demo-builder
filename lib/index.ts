fb9c7b1e07a1f38a9a9b9a631da3e4e11c1f1d83
Steven Guggenheimer <sguggenh@amazon.com>
2024-11-17 17:01:39 +0000

    Update

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,3 @@
 export { OpeaEksCluster } from './construct/resources/cluster';
+export { ImportedCluster as OpeaManifests } from "./construct/resources/imported";
+export { KubernetesModule } from "./construct/modules/kubernetes-module";
