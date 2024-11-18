7319f72522b0dad1a4ecc64fba66b41aa441bfcf
Steven Guggenheimer <sguggenh@amazon.com>
2024-11-18 01:41:28 +0000

    Update change set for manifest stacks

--- lib/app/stacks/eks-stack.ts
+++ lib/app/stacks/eks-stack.ts
@@ -14,22 +14,6 @@ export class OpeaEksStack extends Stack {
         generateBootstrapVersionRule: false
       })
     });
-
-    const bucket = new Bucket(this, "bucket");
-    const parameterName = process.env.IS_WORKSHOP === "workshop" ? "workshop-bucket" : "template-bucket";
-    new StringParameter(this, "bucket-param", {
-      parameterName,
-      stringValue: bucket.bucketName
-    });
-    new StringParameter(this, "command-param", {
-      parameterName: 'get-repo-command-args',
-      stringValue: `s3://${bucket.bucketName}/opea-workshop-builder.zip ./opea-workshop-builder.zip`
-    });
-    // get repo command `aws s3 sync $(aws ssm get-parameter --name get-repo-command-args --query Parameter.Value --output text)`
-    new CfnOutput(this, "bucket-output", {
-      exportName: "get-repo-command",
-      value: `aws s3 cp s3://${bucket.bucketName}/opea-workshop-builder.zip ./opea-workshop-builder.zip`
-    })
     
     this.root = new OpeaEksCluster(this, "OpeaEksCluster", {
       moduleName: 'ChatQnA',
