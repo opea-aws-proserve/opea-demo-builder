7e203305d247e9c44b633d4fa00ca37a0129a756
Steven Guggenheimer <sguggenh@amazon.com>
2024-11-21 20:10:06 +0000

    Add filepath to docker image assets

--- lib/app/bin/start.sh
+++ lib/app/bin/start.sh
@@ -11,6 +11,6 @@ else
     cdk bootstrap
     cdk deploy --require-approval never OpeaEksStack OpeaChatQnAStack
     cdk deploy OpeaGuardrailsStack --require-approval never --method prepare-change-set --change-set-name guardrails-change-set
-#   cdk deploy OpeaOpensearchStack --require-approval never --method prepare-change-set --change-set-name opensearch-change-set
+    cdk deploy OpeaOpensearchStack --require-approval never --method prepare-change-set --change-set-name opensearch-change-set
 
 fi
