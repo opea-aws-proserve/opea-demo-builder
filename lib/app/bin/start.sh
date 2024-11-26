2b0b8d92bf9250f21936ebbdd18f7186737a3640
Steven Guggenheimer <sguggenh@amazon.com>
2024-11-26 20:52:40 +0000

    Update

--- lib/app/bin/start.sh
+++ lib/app/bin/start.sh
@@ -9,10 +9,10 @@ else
     export OPEA_DEMO_BUILDER="https://github.com/opea-aws-proserve/opea-demo-builder.git"
     cdk synth --quiet
     cdk bootstrap
-    cdk deploy --require-approval never OpeaEksStack OpeaChatQnAStack
-    cdk deploy OpeaGuardrailsStack --require-approval never --method prepare-change-set --change-set-name guardrails-change-set
-    cdk deploy OpeaOpensearchStack --require-approval never --method prepare-change-set --change-set-name opensearch-change-set
-    cdk deploy OpeaBedrockStack --require-approval never --method prepare-change-set --change-set-name opensearch-change-set
-#    cdk deploy --all --require-approval never --method prepare-change-set --change-set-name chatqna-change-set
+#    cdk deploy --require-approval never OpeaEksStack OpeaChatQnAStack
+#    cdk deploy OpeaGuardrailsStack --require-approval never --method prepare-change-set --change-set-name guardrails-change-set
+#    cdk deploy OpeaOpensearchStack --require-approval never --method prepare-change-set --change-set-name opensearch-change-set
+#    cdk deploy OpeaBedrockStack --require-approval never --method prepare-change-set --change-set-name opensearch-change-set
+    cdk deploy --all --require-approval never --method prepare-change-set --change-set-name chatqna-change-set
 
 fi
