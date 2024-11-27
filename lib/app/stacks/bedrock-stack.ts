92a9e5f1d27aa8178cbd4570b029c9fb443e7f7f
smguggen <sguggenh@amazon.com>
2024-11-26 20:07:00 -0600

    Update bedrock stack

--- lib/app/stacks/bedrock-stack.ts
+++ lib/app/stacks/bedrock-stack.ts
@@ -1,9 +1,10 @@
 import { DefaultStackSynthesizer, Fn, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Cluster, ClusterAttributes } from 'aws-cdk-lib/aws-eks';
+import { CfnPodIdentityAssociation, Cluster, ClusterAttributes } from 'aws-cdk-lib/aws-eks';
 import { bedrockOverrides } from '../constants';
 import { ImportedCluster } from '../../construct/resources/imported';
 import { join } from 'path';
+import { Effect, PolicyDocument, PolicyStatement, Role, ServicePrincipal, SessionTagsPrincipal } from 'aws-cdk-lib/aws-iam';
 
 // NOTE: Before using this stack you must enable the model in the region you're using in the AWS account
 export class OpeaBedrockStack extends Stack {
@@ -16,7 +17,35 @@ export class OpeaBedrockStack extends Stack {
       })
     });
 
-    const importedCluster = new ImportedCluster(this, `opensearch-imported`, {
+     // Set up role to allow pod to access Bedrock
+     const bedrockPodIdentityRole = new Role(this, 'BedrockPodIdentityRole', {
+      assumedBy: new SessionTagsPrincipal(new ServicePrincipal('pods.eks.amazonaws.com')),
+      inlinePolicies: {
+        InvokeBedrock: new PolicyDocument({
+          statements: [
+            new PolicyStatement({
+              actions: [
+                "bedrock:InvokeModel",
+                "bedrock:InvokeModelWithResponseStream"
+              ],
+              effect: Effect.ALLOW,
+              resources: ["*"],
+            })
+          ]
+        })
+      }
+    });
+
+    // Set up pod identity association to allow Bedrock pod to use the role
+    const bedrockPodIdentityAssociation = new CfnPodIdentityAssociation(this, 'BedrockPodIdentity', {
+      clusterName: cluster.clusterName,
+      namespace: 'bedrock',
+      roleArn: bedrockPodIdentityRole.roleArn,
+      serviceAccount: 'chatqna-bedrock-sa'
+    });
+
+
+    const importedCluster = new ImportedCluster(this, `bedrock-imported`, {
       moduleName:'ChatQnA',
       cluster,
       skipPackagedManifests: true,
@@ -43,5 +72,6 @@ export class OpeaBedrockStack extends Stack {
         }
       ]
     });
+    importedCluster.node.addDependency(bedrockPodIdentityAssociation);
   }
 }
