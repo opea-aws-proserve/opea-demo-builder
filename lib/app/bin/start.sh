222d11f99c9c350c6d9da523194d798a5bf393b8
Steven Guggenheimer <sguggenh@amazon.com>
2024-11-30 14:28:43 +0000

    Update change sets

--- lib/app/bin/start.sh
+++ lib/app/bin/start.sh
@@ -9,9 +9,9 @@ else
     export OPEA_DEMO_BUILDER="https://github.com/opea-aws-proserve/opea-demo-builder.git"
     cdk synth --quiet
     cdk bootstrap
-    cdk deploy --require-approval never OpeaEksStack OpeaChatQnAStack 
-#    cdk deploy OpeaEksStack --require-approval never --method prepare-change-set --change-set-name eks-change-set
-#    cdk deploy OpeaChatQnAStack --require-approval never --method prepare-change-set --change-set-name default-change-set
+#    cdk deploy --require-approval never OpeaEksStack OpeaChatQnAStack 
+    cdk deploy OpeaEksStack --require-approval never --method prepare-change-set --change-set-name eks-change-set
+    cdk deploy OpeaChatQnAStack --require-approval never --method prepare-change-set --change-set-name default-change-set
     cdk deploy OpeaGuardrailsStack --require-approval never --method prepare-change-set --change-set-name guardrails-change-set
     cdk deploy OpeaOpensearchStack --require-approval never --method prepare-change-set --change-set-name opensearch-change-set
     cdk deploy OpeaBedrockStack --require-approval never --method prepare-change-set --change-set-name bedrock-change-set
