967ef20607d4daa22198fc63e479d79c1ad17aa6
smguggen <sguggenh@amazon.com>
2024-12-17 17:35:06 -0600

    Update

--- lib/app/bin/start.sh
+++ lib/app/bin/start.sh
@@ -8,7 +8,7 @@ if [[ "$STACK_OPERATION" == "Create" || "$STACK_OPERATION" == "Update" ]]; then
     export OPEA_DEMO_BUILDER="https://github.com/opea-aws-proserve/opea-demo-builder.git"
     export AWS_ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
     cdk synth --quiet
-    cdk bootstrap aws://$AWS_ACCOUNT/$AWS_REGION --public-access-block-configuration false --qualifier opea --toolkit-stack-name OpeaBootstrap
+#    cdk bootstrap aws://$AWS_ACCOUNT/$AWS_REGION --public-access-block-configuration false --qualifier opea --toolkit-stack-name OpeaBootstrap
     cdk deploy --require-approval never OpeaEksStack OpeaChatQnAStack 
 #    cdk deploy OpeaEksStack --require-approval never --method prepare-change-set --change-set-name eks-change-set
 #    cdk deploy OpeaChatQnAStack --require-approval never --method prepare-change-set --change-set-name default-change-set
