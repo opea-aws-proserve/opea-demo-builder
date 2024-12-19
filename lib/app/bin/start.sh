3bb7aba106a9d8905f7b91707021e09500af0236
smguggen <sguggenh@amazon.com>
2024-12-19 10:16:05 -0600

    Set ChatQnA Stack to stop before executing change set

--- lib/app/bin/start.sh
+++ lib/app/bin/start.sh
@@ -9,9 +9,9 @@ if [[ "$STACK_OPERATION" == "Create" || "$STACK_OPERATION" == "Update" ]]; then
     export AWS_ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
 #    cdk synth --quiet
     cdk bootstrap aws://$AWS_ACCOUNT/$AWS_REGION --public-access-block-configuration false
-    cdk deploy --require-approval never OpeaEksStack OpeaChatQnAStack 
+    cdk deploy --require-approval never OpeaEksStack
 #    cdk deploy OpeaEksStack --require-approval never --method prepare-change-set --change-set-name eks-change-set
-#    cdk deploy OpeaChatQnAStack --require-approval never --method prepare-change-set --change-set-name default-change-set
+    cdk deploy OpeaChatQnAStack --require-approval never --method prepare-change-set --change-set-name default-change-set
     cdk deploy OpeaGuardrailsStack --require-approval never --method prepare-change-set --change-set-name guardrails-change-set
     cdk deploy OpeaOpensearchStack --require-approval never --method prepare-change-set --change-set-name opensearch-change-set
     cdk deploy OpeaBedrockStack --require-approval never --method prepare-change-set --change-set-name bedrock-change-set
